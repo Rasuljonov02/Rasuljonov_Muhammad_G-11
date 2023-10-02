5468f7d4de2e0a249cfb949e985417c1090598b5
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-10-02 08:11:36 +0500

    f

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-// Selecting various HTML elements using their classes
+
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
 const btn3 = document.querySelector(".btn3");
 const todoo = document.querySelector(".todoo");
 const inprogress = document.querySelector(".inprogres");
 const done = document.querySelector(".done");
-const droppables = document.querySelectorAll(".todo"); // Drop zones
-const draggables = document.querySelectorAll(".task"); // Draggable tasks
+const droppables = document.querySelectorAll(".todo");
+const draggables = document.querySelectorAll(".task");
 
-// Adding drag and drop event listeners to draggable tasks
+
 draggables.forEach((task) => {
 	task.addEventListener("dragstart", () => {
 		task.classList.add("is-dragging");
@@ -20,16 +20,15 @@ draggables.forEach((task) => {
 	});
 });
 
-// Adding dragover event listeners to drop zones
+
 droppables.forEach((zone) => {
 	zone.addEventListener("dragover", (e) => {
 		e.preventDefault();
 
-		// Determine the task above which the dragged task will be inserted
 		const bottomTask = insertAboveTask(zone, e.clientY);
 		const curTask = document.querySelector(".is-dragging");
 
-		// Insert the dragged task either below or above another task
+
 		if (!bottomTask) {
 			zone.appendChild(curTask);
 		} else {
@@ -38,7 +37,6 @@ droppables.forEach((zone) => {
 	});
 });
 
-// Function to find the closest task above the mouse pointer's position
 const insertAboveTask = (zone, mouseY) => {
 	const els = zone.querySelectorAll(".task:not(.is-dragging)");
 
@@ -62,11 +60,10 @@ let i = 4;
 let ii = 4;
 let iii = 4;
 
-// Event listener for the first button (btn1)
 btn1.addEventListener("click", (e) => {
 	i++;
 	let create = document.createElement("p");
-	create.className = "task"; // Changed the class name to "task"
+	create.className = "task";
 	create.draggable = true;
 	create.innerText = `todo #${i}`;
 	todoo.appendChild(create);
@@ -79,11 +76,10 @@ btn1.addEventListener("click", (e) => {
 	});
 });
 
-// Event listener for the second button (btn2)
 btn2.addEventListener("click", (e) => {
 	ii++;
 	let create = document.createElement("p");
-	create.className = "task"; // Changed the class name to "task"
+	create.className = "task";
 	create.draggable = true;
 	create.innerText = `IP #${ii}`;
 	inprogress.appendChild(create);
@@ -96,11 +92,10 @@ btn2.addEventListener("click", (e) => {
 	});
 });
 
-// Event listener for the third button (btn3)
 btn3.addEventListener("click", (e) => {
 	iii++;
 	let create = document.createElement("p");
-	create.className = "task"; // Changed the class name to "task"
+	create.className = "task";
 	create.draggable = true;
 	create.innerText = `Done #${iii}`;
 	done.appendChild(create);
